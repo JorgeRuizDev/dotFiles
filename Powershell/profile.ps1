35f868e1a58cc3865c39cbc0b2baea651ee8fb21
Jorge Ruiz <jorgelinirg@gmail.com>
2020-11-07 12:25:44 +0100

    first

---
+++ Powershell/profile.ps1
@@ -0,0 +1,11 @@
+Enum Computers {
+	Desktop = 1
+	Laptop = 2
+}
+
+Get-Date
+
+function conda_sh{
+	powershell -ExecutionPolicy ByPass -NoExit -Command "& 'C:\Users\Jorge\anaconda3\shell\condabin\conda-hook.ps1' ; conda activate 'C:\Users\Jorge\anaconda3' "
+}
+Set-Alias condash conda_sh
